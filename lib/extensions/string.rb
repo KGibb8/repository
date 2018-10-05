4534d2a1942fcf642823359072e3caaffed23041
Kieran <kieran@tenthousandthings.org.uk>
2018-10-05 13:13:37 +0100

    correct string underscore patch

--- lib/extensions/string.rb
+++ lib/extensions/string.rb
@@ -11,9 +11,7 @@ module Extensions
     end
 
     def underscore
-      self.gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
-      .gsub(/([a-z\d])([A-Z])/,'\1_\2')
-      .downcase
+      self.scan(/[A-Z][a-z]+/).join('_').downcase
     end
 
     def singularize
